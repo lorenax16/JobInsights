4630c8980e50f3881e15c661d3de82ad9bb98afa
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-12-12 13:20:18 -0300

    req 1 função para ler o arquivo csv

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,56 +1,39 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-    
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
-
+    with open(path) as csv_file:
+      jobs = csv.DictReader(csv_file)
+      jobs_list = []
+      for job in jobs:
+        jobs_list.append(job)
+      return jobs_list
+print(read("data/jobs.csv"))
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+   jobs = read(path)
+   list_jobs = []
+   for job in jobs:
+    list_jobs.append(job["job_type"])
+   return set(list_jobs)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    # """Filters a list of jobs by job_typ
+    # Parameters
+    # ----------
+    # jobs : list
+    #     List of jobs to be filtered
+    # job_type : str
+    #     Job type for the list filter
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    # Returns
+    # -------
+    # list
+    #     List of jobs with provided job_type
+    # """
+    # raise NotImplementedError
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
