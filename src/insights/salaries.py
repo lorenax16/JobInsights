4e3daf966cec901d46ca6717fae1bea332b2f26b
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-12-13 09:45:39 -0300

    requisitos de teste

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -37,18 +37,8 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    if matches_salary_range(jobs, salary):
+        data = []
+        data.append(jobs)
+    return data
