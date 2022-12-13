0de8fafad4856e776eb1ae2133f92d34a49a3e21
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-12-13 09:59:30 -0300

    requisitos de teste 2

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    file = "tests/mocks/brazilians_jobs.csv"
+    fun = read_brazilian_file(file)[0].keys()
+    assert "salary" in fun
+    assert "title" in fun
+    assert "type" in fun
